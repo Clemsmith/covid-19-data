fef4d185cc30a13b2bc09d8de28a4524c1494db5
Clemsmith <92476113+Clemsmith@users.noreply.github.com>
2023-10-22 14:40:04 +0100

    Delete scripts directory

--- scripts/src/cowidev/hosp/countries.py
+++
@@ -1,6 +0,0 @@
-from cowidev.hosp.sources import __all__ as countries
-
-
-# Import modules
-country_to_module = {c: f"cowidev.hosp.sources.{c}" for c in countries}
-MODULES_NAME = list(country_to_module.values())
